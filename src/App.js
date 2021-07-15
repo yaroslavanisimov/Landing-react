2cc98de1891ea7fabdde9c70273be5f35ca408ca
YaroslavAnisimov <yaroslavanisimow@yandex.ru>
2021-07-16 00:31:28 +0200

    landing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 
+import './style.css';
+
+// const app = React.createElement('div', {className: 'App'}, 'Hello');
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( 
+    <React.Fragment>
+    <Header/>
+    <Main/>
+    </React.Fragment>
   );
 }
 
