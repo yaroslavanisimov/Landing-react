601e1553543c1807b6972003ddc7e49e713d85bc
YaroslavAnisimov <yaroslavanisimow@yandex.ru>
2021-07-19 18:27:25 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import Features from './components/Features/Features';
+import Footer from './components/Footer/Footer';
+import Calendar from './components/Calendar/Calendar';
+import Details from './components/Details/Details';
 
 import './style.css';
 
@@ -9,8 +13,11 @@ import './style.css';
 function App() {
   return ( 
     <React.Fragment>
-    <Header/>
-    <Main/>
+      <Header/>
+      <Main/>
+      <Features/>
+      <Footer/>
+      <Calendar/>
     </React.Fragment>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
 // react.strictmode start the layout after the rendering
 
 ReactDOM.render(
